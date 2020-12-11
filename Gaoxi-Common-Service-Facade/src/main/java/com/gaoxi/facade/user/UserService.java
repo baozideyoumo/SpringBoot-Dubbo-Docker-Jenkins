eac026f7beccc191897269d6f0272dec1994d6a3
卢潇毅 <deathlxy@vip.qq.com>
2020-12-11 17:39:08 +0800

    新增注解

--- Gaoxi-Common-Service-Facade/src/main/java/com/gaoxi/facade/user/UserService.java
+++ Gaoxi-Common-Service-Facade/src/main/java/com/gaoxi/facade/user/UserService.java
@@ -2,10 +2,11 @@ package com.gaoxi.facade.user;
 
 import com.gaoxi.entity.user.*;
 import com.gaoxi.req.user.*;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-
+@Service
 public interface UserService {
 
     public UserEntity login(LoginReq loginReq);
